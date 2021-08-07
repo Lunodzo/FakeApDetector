d792c38fc0ae52389e120cdbca62a60786d1dc2b
lunodzo <lunomwinuka@gmail.com>
2021-08-07 04:43:08 +0300

    Scanning ten rounds works

--- app/src/main/java/net/kismetwireless/android/pcapcapture/WiFiScan.java
+++ app/src/main/java/net/kismetwireless/android/pcapcapture/WiFiScan.java
@@ -1,14 +1,19 @@
 package net.kismetwireless.android.pcapcapture;
 
-import static android.os.SystemClock.sleep;
-
+import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -18,6 +23,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +36,7 @@ public class WiFiScan extends AppCompatActivity {
    Button scanButton, scanEvilButton;
    ArrayList<String> arrayList = new ArrayList<>();
    ArrayAdapter arrayAdapter;
+   public int count = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,15 @@ public class WiFiScan extends AppCompatActivity {
         setContentView(R.layout.activity_wi_fi_scan);
         scanButton = findViewById(R.id.btn_List);
         scanEvilButton = findViewById(R.id.btn_scan_evil);
+        wifiList = findViewById(R.id.wifiList);
+        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+
+        //Delete Data about scanned APs if they exist, this allows to start a fresh scan
+        DatabaseHandler databaseHandler = new DatabaseHandler(getApplicationContext());
+        databaseHandler.deleteApContents();
+
+        //Button to scan Wifi
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,16 +63,15 @@ public class WiFiScan extends AppCompatActivity {
             }
         });
 
+        //Button to scan Fake APs
         scanEvilButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                scanEvilWiFi();
+                //Second button actions
+                //scanEvilWiFi();
             }
         });
 
-        wifiList = findViewById(R.id.wifiList);
-        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        
         if (!wifiManager.isWifiEnabled()){
             Toast.makeText(this, "Enable WiFi", Toast.LENGTH_SHORT).show();
             wifiManager.setWifiEnabled(true);
@@ -67,89 +82,6 @@ public class WiFiScan extends AppCompatActivity {
         scanWiFi();
     }
 
-    private void scanEvilWiFi() {
-        arrayList.clear();
-        registerReceiver(wifiEvilReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-
-
-        int scanLoop = 2;
-        List<ScanResult> ssid;
-        List<ScanResult> bssid;
-        List<ScanResult> signal;
-        List<ScanResult> capabilities;
-
-        String firstSSID;
-        String secondSSID;
-        String thirdSSID;
-
-        String firstBSSID;
-        String secondBSSID;
-        String thirdBSSID;
-
-        int firstSignal;
-        int secondSignal;
-        int thirdSignal;
-
-        String firstCapabilities;
-        String secondCapabilities;
-        String thirdCapabilities;
-
-//        String [] bssid = new String[10];
-//        int [] signal = new int[10];
-//        String [] capabilities = new String[10];
-        Toast.makeText(this, "Scanning three times to collect enough results..", Toast.LENGTH_SHORT).show();
-
-        //Control number of scans
-        for (int i = 0; i <= scanLoop; i++){
-            //Loop through all results and try to store each in a separate variable
-            //Remember: You may have more that one SSID and everything else
-            //TODO create a list of SSID, BSSID, signal strength and capabilities in this loop
-            for(ScanResult scanResult: results) {
-                wifiManager.startScan();
-                //TODO Chech if the startScan() was necessary here
-
-                if(i == 0){
-                    Toast.makeText(this, "First scan", Toast.LENGTH_SHORT).show();
-                    //firstSSID = Collections.singletonList(arrayList.add(scanResult.SSID));
-                    //Loop controls number of results from one scan
-                    for (int j=0; j < results.size(); j++){
-                        firstSSID = results.get(j).SSID;
-                        firstBSSID = results.get(j).BSSID;
-                        firstSignal = results.get(j).level;
-                        firstCapabilities = results.get(j).capabilities;
-
-                        //Ignore these two lines
-                        arrayList.add(firstSSID);
-                        Object[] collectedFisrtSSID = arrayList.toArray();
-
-                        //Testing what is being stored
-                        Toast.makeText(this, "Found "+firstSSID, Toast.LENGTH_SHORT).show();
-                    }
-
-                    //Toast.makeText(this, "Matokeo ni "+firstSSID+" "+firstBSSID+" "
-                            //+firstSignal+" "+firstCapabilities, Toast.LENGTH_SHORT).show();
-                }else if(i == 1){
-                    Toast.makeText(this, "Second scan", Toast.LENGTH_SHORT).show();
-                    secondSSID = results.get(i).SSID;
-                    secondBSSID = results.get(i).BSSID;
-                    secondSignal = results.get(i).level;
-                    secondCapabilities = results.get(i).capabilities;
-                }else if (i == 2){
-                    Toast.makeText(this, "Third scan", Toast.LENGTH_SHORT).show();
-                    //thirdSSID = results.get(i).SSID;
-                    //thirdBSSID = results.get(i).BSSID;
-                    //thirdSignal = results.get(i).level;
-                    //thirdCapabilities = results.get(i).capabilities;
-                }else{
-                    Toast.makeText(this, "Out of boundary", Toast.LENGTH_SHORT).show();
-                }
-
-                //Toast.makeText(this, "Matokeo ni "+ssid[scanLoop]+" "+firstBSSID+" "
-                    //+firstSignal+" "+firstCapabilities, Toast.LENGTH_SHORT).show();
-            }
-            sleep(3000);
-        }
-    }
 
     private  void scanWiFi(){
         arrayList.clear();
@@ -158,11 +90,51 @@ public class WiFiScan extends AppCompatActivity {
         Toast.makeText(this, "Scanning..", Toast.LENGTH_SHORT).show();
     }
 
+
+
     BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             results = wifiManager.getScanResults();
-            unregisterReceiver(this);
+            int size = results.size();
+            DatabaseHandler databaseHandler = new DatabaseHandler(context);
+
+            //Check if there is any AP
+            if (size > 0) {
+                //Loop according to the number of APs OR for each available AP, post the details into
+                //DB
+                for (int i = 0; i < size; i++) {
+                    ScanResult scanResult = wifiManager.getScanResults().get(i);
+
+                    //AP parameters
+                    String ssid = scanResult.SSID;
+                    String bssid = scanResult.BSSID;
+                    int rssi = scanResult.level;
+                    String capabilities = scanResult.capabilities;
+                    java.util.Date date = new java.util.Date();
+                    java.sql.Date currentTime = new java.sql.Date(date.getTime());
+                    SimpleDateFormat dft = new SimpleDateFormat("HH:mm:ss");
+                    String time = dft.format(currentTime);
+
+                    //Write a statement to post these into Database
+                    boolean insert = databaseHandler.addAccessPoints(ssid, bssid, rssi, capabilities, count, false, time);
+                    if (insert == true){
+                        Toast.makeText(getApplicationContext(), count+" Round Data inserted", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Data insert Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                count++;
+                if(count <= 10){
+                    wifiManager.startScan();
+                }else{
+                    unregisterReceiver(this);
+                }
+            }else{
+                unregisterReceiver(this);
+                Toast.makeText(getApplicationContext(), "Scan Failed", Toast.LENGTH_SHORT).show();
+            }
+
 
             for (ScanResult scanResult: results){
                 arrayList.add(scanResult.SSID +" * "+ scanResult.capabilities + " * "+ scanResult.BSSID+ " * "
@@ -172,11 +144,101 @@ public class WiFiScan extends AppCompatActivity {
         }
     };
 
-    BroadcastReceiver wifiEvilReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            results = wifiManager.getScanResults();
+    public class WifiScanAsyncTask extends AsyncTask<Void, String, String> {
+        ProgressDialog dg = new ProgressDialog(getApplicationContext());
+
+        protected void onPreExecute(){
+            dg.setMessage("Scanning async");
+            dg.setIndeterminate(false);
+            dg.setCancelable(false);
+            dg.show();
         }
-    };
+
+        protected void onPostExecute(Void result) {
+            dg.dismiss();
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            scanWiFi();
+            return null;
+        }
+    }
+
+    public class DatabaseHandler extends SQLiteOpenHelper{
+        private static final String TAG = "DatabaseHelper";
+        private static final int DATABASE_VERSION = 1;
+        private static final String DATABASE_NAME = "fakeapdetect";
+        private static final String TABLE_AP = "accesspoints";
+        private static final String ID = "id";
+        private static final String SSID = "ssid";
+        private static final String BSSID = "bssid";
+        private static final String SIGNAL = "level";
+        private static final String CAPABILITIES = "capabilities";
+        private static final String SCAN_ROUND = "round";
+        private static final String COMMENT = "comment";
+        private static final String TIME = "time";
+        Time time = new Time();
+
+        public DatabaseHandler(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String CREATE_AP_TABLE = "CREATE TABLE " + TABLE_AP + "("
+                    + ID + " INTEGER PRIMARY KEY,"
+                    + SSID + " TEXT,"
+                    + BSSID + " TEXT,"
+                    + SIGNAL + " INTEGER,"
+                    + CAPABILITIES + " TEXT,"
+                    + SCAN_ROUND + " INTEGER,"
+                    + COMMENT + " BOOLEAN,"
+                    + TIME + " TEXT"
+                    + ")";
+            db.execSQL(CREATE_AP_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_AP);
+            onCreate(db);
+        }
+
+        //Add AP to Database
+        public boolean addAccessPoints(String ssid, String bssid, int signal, String capabilities,
+                                       int round, boolean comment, String time){
+            SQLiteDatabase db = this.getWritableDatabase();
+
+
+            ContentValues values = new ContentValues();
+            values.put(SSID, ssid);
+            values.put(BSSID, bssid);
+            values.put(SIGNAL, signal);
+            values.put(CAPABILITIES, capabilities);
+            values.put(SCAN_ROUND, round);
+            values.put(COMMENT, comment);
+            values.put(TIME, String.valueOf(time));
+
+            Log.d(TAG, "addAccessPoints: Adding " +ssid+ " and "+bssid+" to "+TABLE_AP);
+            long result = db.insert(TABLE_AP, null, values);
+
+            if (result == -1){
+                return false;
+            }else{
+                return true;
+            }
+        }
+
+        public void deleteApContents(){
+            SQLiteDatabase db = this.getWritableDatabase();
+            String deleteContent = "DELETE FROM accesspoints";
+            db.execSQL(deleteContent);
+        }
+
+        public void getStoredAPs(){
+            //TODO Fetch data from database then do the logic
+        }
+    }
 }
 
