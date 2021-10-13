5ec06b31dab82fd9744996224150efb6571c3f3d
lunodzo <lunomwinuka@gmail.com>
2021-10-13 14:26:37 +0300

    Http error responses

--- app/src/main/java/net/kismetwireless/android/pcapcapture/CaptivePortal.java
+++ app/src/main/java/net/kismetwireless/android/pcapcapture/CaptivePortal.java
@@ -13,8 +13,24 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
 public class CaptivePortal extends Activity {
-    String url = "https://fakeap.lunodzo.com";
+    String url = "http://fakeap.lunodzo.com";
     //String url = "https://sis.nm-aist.ac.tz/student/student.php";
+
+    //Method to generate string
+//    static String randomString(int k){
+//        String RandomString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//                + "0123456789"
+//                + "abcdefghijklmnopqrstuvxyz";
+//        // create StringBuffer size of AlphaNumericString
+//        StringBuilder sb = new StringBuilder(10);
+//
+//        for (int i=0; i<10; i++){
+//            int index = (int) (randomString(10).length()*Math.random());
+//            sb.append(RandomString.charAt(index));
+//        }
+//        return sb.toString();
+//    }
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -32,27 +48,31 @@ public class CaptivePortal extends Activity {
 
             public void onReceivedError(WebView view, int errorCode, String desc, String failUrl){
                 webView.loadUrl("https://sis.nm-aist.ac.tz/");
-                Toast.makeText(getApplicationContext(), "Code received "+errorCode, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), "Code received "+errorCode, Toast.LENGTH_SHORT).show();
             }
         });
 
         webView.loadUrl(url);
         webView.getSettings().setJavaScriptEnabled(true);
 
-        webView.loadUrl("javascript:document.getElementsByName('username').value = 'username'");
-        webView.loadUrl("javascript:document.getElementsByName('password').value = 'password'");
-        webView.loadUrl("javascript:document.forms['login'].submit()");
+        //Generate random credentials
+        //String username = CaptivePortal.randomString(10);
+        String username = "Abcdefghhrj";
+        //String password = CaptivePortal.randomString(10);
+        String password = "BDHHSD78678";
+        webView.loadUrl("javascript:document.getElementsByName('username').value = "+username);
+        webView.loadUrl("javascript:document.getElementsByName('password').value = "+password);
+        webView.loadUrl("javascript:document.forms['submit'].submit()");
 
         //Read HTTP response
         WebResourceResponse errorResponse = null;
         WebResourceRequest request;
-        //TODO Capture error codes after submiting
-//        int statusCode = errorResponse.getStatusCode();
-//        Toast.makeText(getApplicationContext(), "Hello status code is "+statusCode, Toast.LENGTH_SHORT).show();
-//        if(statusCode == 200){
-//            //Do something...
-//        }else{
-//            //Do something else...
-//        }
+        //Capture error coded from HTTP response after submitting the credentials
+        int statusCode = errorResponse.getStatusCode();
+        if(statusCode != 401){
+            Toast.makeText(getApplicationContext(), "Fake Captive Portal"+statusCode, Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "Legit Captive Portal"+statusCode, Toast.LENGTH_SHORT).show();
+        }
     }
 }
